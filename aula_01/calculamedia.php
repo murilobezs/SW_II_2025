15ae1b6fad619903f1d52a88c141861f66b9d2b0
murilobezs <givewaybymf@gmail.com>
2025-02-12 08:12:04 -0300

    hi

--- aula_01/calculamedia.php
+++ aula_01/calculamedia.php
@@ -1,13 +1,17 @@
-<?php 
-$nota1 =
-$nota2 =
-$nota3 = 
-$media = 
+<?php
+
+$nota1 = 7;
+$nota2 = 8;
+$nota3 = 9;
+
+
+$media = ($nota1 + $nota2 + $nota3) / 3;
+
+
 
 echo "Nota 1: " . $nota1 . "<br/>";
 echo "Nota 2: " . $nota2 . "<br/>";
 echo "Nota 3: " . $nota3 . "<br/>";
 
 echo "Média: " . $media . "<br/>";
-
 ?>
