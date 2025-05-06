c629d517b6fe2ac50241fefe9a30837cd0b7ed39
murilobezs <givewaybymf@gmail.com>
2025-05-06 08:52:42 -0300

    aula 10 | 06.05.2025 | finalizado

--- aula_10/api.php
+++ aula_10/api.php
@@ -80,6 +80,72 @@
             ], JSON_UNESCAPED_UNICODE);            
             break;        
 
+        case 'DELETE':
+            if(!isset($_GET['id'])) {
+                http_response_code(400);
+                echo json_encode(["erro" => "ID é obrigatório"], JSON_UNESCAPED_UNICODE);
+                exit;
+            }
+
+            $id = intval($_GET['id']);
+            $indice = -1;
+
+            foreach ($usuarios as $i => $usuario) {
+                if($usuario['id'] == $id){
+                    $indice = $i;
+                    break;
+                }
+            }
+
+            if ($indice >= 0) {
+                array_splice($usuarios, $indice, 1);
+                file_put_contents($arquivo, json_encode($usuarios, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+                echo json_encode(["mensagem" => "Usuário removido com sucesso"], JSON_UNESCAPED_UNICODE);
+            } else {
+                http_response_code(404);
+                echo json_encode(["erro" => "Usuário não encontrado"], JSON_UNESCAPED_UNICODE);
+            }
+            break;
+
+        case 'PUT':
+            if(!isset($_GET['id'])) {
+                http_response_code(400);
+                echo json_encode(["erro" => "ID é obrigatório"], JSON_UNESCAPED_UNICODE);
+                exit;
+            }
+
+            $dados = json_decode(file_get_contents('php://input'), true);
+            
+            if (!isset($dados["nome"]) || !isset($dados["email"])) {
+                http_response_code(400);
+                echo json_encode(["erro" => "Nome e email são obrigatórios!!"], JSON_UNESCAPED_UNICODE);
+                exit;
+            }
+
+            $id = intval($_GET['id']);
+            $usuario_atualizado = false;
+
+            foreach ($usuarios as &$usuario) {
+                if($usuario['id'] == $id){
+                    $usuario['nome'] = $dados["nome"];
+                    $usuario['email'] = $dados["email"];
+                    $usuario_atualizado = true;
+                    break;
+                }
+            }
+
+            if ($usuario_atualizado) {
+                file_put_contents($arquivo, json_encode($usuarios, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+                echo json_encode([
+                    "mensagem" => "Usuário atualizado com sucesso",
+                    "usuario" => $dados
+                ], JSON_UNESCAPED_UNICODE);
+            } else {
+                http_response_code(404);
+                echo json_encode(["erro" => "Usuário não encontrado"], JSON_UNESCAPED_UNICODE);
+            }
+            break;
+
         default:
             http_response_code(405); //metodo n permitido
             echo json_encode(["erro" => "Método não permitido"], JSON_UNESCAPED_UNICODE);
